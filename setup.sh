a667a7deef92f2c6e48651fa5ed8d051bbbc1005
Chigozie Godwin <godwinchigozie679@gmail.com>
2021-11-23 12:05:59 -0500

    recent

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 mkdir -p ~/.streamlit/
-
+echo "\
+[general]\n\
+email = \"godwinchigozie679@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
 headless = true\n\
-\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
